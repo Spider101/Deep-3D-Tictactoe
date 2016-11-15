d5317882cbc7a6742e986736af6c4ae61d3af506
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2016-11-15 00:25:48 -0500

    added determine function function

--- 2dminimax.py
+++ 2dminimax.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 __author__ = "Harshvardhan Gupta"
+import copy
 
 
 win_states = (
@@ -22,7 +23,7 @@ class Tic(object):
         if len(positions) == 0:
             self.positions = [None for i in range(9)]
         else:
-            self.positions = positions
+            self.positions = copy.deepcopy(positions)
 
     def display(self):
         print("")
@@ -45,20 +46,75 @@ class Tic(object):
         for i in range(9):
             if self.positions[i]==None:
                 possibleStates.append(Tic(self.side,self.positions))
-                possibleStates[c].positions[i] = self.side
-                possibleStates[c].display()
+                possibleStates[-1].positions[i] = self.side
+                # possibleStates[-1].display()
+
+
+        return possibleStates
+
+    def getOppPlayer(self):
+        if self.side =="o" :
+            return "x"
+        else:
+            return "o"
+
+    def caclulateScore(self):
+        for win_state in enumerate(win_states):
+
+            if self.positions[win_state[1][0]] == self.side and self.positions[win_state[1][1]] ==self.side and self.positions[win_state[1][2]] == self.side :
+                return 1
+            elif self.positions[win_state[1][0]] == self.getOppPlayer() and self.positions[win_state[1][1]] ==self.getOppPlayer() and self.positions[win_state[1][2]] == self.getOppPlayer() :
+                return -1
+            else :
+                return 0
+
+    def isGameOver(self):
+        for i in enumerate(self.positions):
+            if i[1] ==None:
+                return False
+        return True
+
+
+def determine(tic):
+    assert (isinstance(tic,Tic))
+
+    if(tic.isGameOver()):
+        return tic
+
+    # tic.display()
+    states = []
+    scores = []
+
+    for state in enumerate(tic.getNextPossibleStates()):
+        score = state[1].caclulateScore()
+        states.append(state[1])
+        scores.append(score)
+
+        if score ==1:
+            return state[1]
+
+
+    for score in enumerate(scores):
+        if score[1] ==0:
+            return states[score[0]]
+
+    return states[0]
 
 
 
 
 
-testBoard = ['x','x','o',None,"o",None,None,None,None]
+
+
+testBoard = [None,None,None,None,'o',None,None,None,None]
 testTic =  Tic("x",testBoard)
 testTic.display()
-print("that was init")
-
-testTic.getNextPossibleStates()
-
+print("displayed")
+# testTic.display()
+# print("that was init")
+# print(testTic.caclulateScore())
+# testTic.getNextPossibleStates()
+determine(testTic).display()
 
 
 

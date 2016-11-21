cb1e3300e9787465392849aa16837bc60230e95c
Abhimanyu Banerjee <ab3cb@power1.cs.virginia.edu>
2016-11-21 15:34:02 -0500

    tidied up the code and started using __future__ for print statements

--- q_learning.py
+++ q_learning.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 __author__ = "Abhimanyu Banerjee"
 
 from random import random, choice
@@ -22,7 +23,7 @@ def print_board(state):
 		for j in xrange(len(state[i])):
 			cell_values.append(symbols[int(state[i][j] + 1)])
 
-	print board_format.format(*cell_values)
+	print(board_format.format(*cell_values))
 
 def open_spots(state):
 	open_cells = []
@@ -68,7 +69,7 @@ def get_state_key(state):
 
 	flat_state = [cell for row in state for cell in row]
 	key = "".join(map(str, flat_state))
-	#print key
+	#print(key)
 	return key
 
 def play(player1, player2):
@@ -83,9 +84,11 @@ def play(player1, player2):
 			symbol = player2.symbol
 		state[i][j] = symbol
 		winner = int(is_game_over(state))
-		if winner != 0: #one of the players
+		if winner != 0: #one of the players won
+			print_board(state)
 			return winner 
 	
+	print_board(state)
 	return winner #nobody won
 
 class Agent(object):
@@ -100,25 +103,30 @@ class Agent(object):
 		self.prev_score = 0
 		self.num_states = 0
 
-		print "\nInitializing state table for player ", self.symbol, ". Please wait ..."
+		print("\nInitializing state table for player ", self.symbol, ". Please wait ...")
 		start_time = timeit.default_timer()
 		self.generate_state_value_table(empty_state(), 0)
-		print "Time taken to initialize state table: ", (timeit.default_timer() - start_time) 
+		print("Time taken to initialize state table: ", (timeit.default_timer() - start_time) )
 
 	def generate_state_value_table(self, state, turn):
 
-		winner = int(is_game_over(state)) #check if for the current turn and state, game has finished and if so who won
-		#print "\nWinner is ", winner
-		#print "\nBoard at turn: ", turn
+		winner = int(is_game_over(state)) #check if, for the current turn and state, game has finished and if so who won
+		#print("\nWinner is ", winner)
+		#print("\nBoard at turn: ", turn)
 		#print_board(state)	
 		self.add_state(state, winner/2 + 0.5) #add the current state with the appropriate value to the state table
+		
+		#check if the winner returned is one of the players and go back to the previous state if so
+		if winner != 0:
+			return
 
-		board_size = len(state)*len(state)
+		open_cells = open_spots(state) #find the index (from 0 to total no. of cells) of all the empty cells in the board 
 
-		open_cells = open_spots(state) 
 		#check if there are any empty cells in the board
 		if len(open_cells) > 0:
+			
 			for cell in open_cells:
+			
 				#pdb.set_trace()
 				row, col = cell / len(state), cell % len(state)
 				new_state = deepcopy(state) #make a copy of the current state 
@@ -134,11 +142,10 @@ class Agent(object):
 					#check if the new state has not been generated somewhere else in the search tree
 					if not self.check_duplicates(new_state):
 						self.generate_state_value_table(new_state, turn+1)
-					else:
-						return
+					
 				except:
-					#print "Recursive depth exceeded"
-					exit()
+					#print("Recursive depth exceeded")
+					exit("Recursive depth exceeded")
 
 		else:
 			return
@@ -151,7 +158,7 @@ class Agent(object):
 		return False
 
 	def add_state(self, state, value):
-		#print "\nAdded state", len(self.state_values) + 1
+		#print("\nAdded state", len(self.state_values) + 1)
 
 		if len(self.state_values) < self.num_states:
 			exit("Duplicate state!")
@@ -164,7 +171,7 @@ class Agent(object):
 
 	def update_state_table(self, val):
 		''' Back up the value to the previous state using ....'''
-		if self.prevstate != None and self.learning:
+		if self.prev_state != None and self.learning:
 			prev_state_key = get_state_key(self.prev_state)
 			prev_score = self.state_values[prev_state_key]
 			self.state_values[prev_state_key] += self.alpha * (nextval - prevscore)
@@ -174,13 +181,13 @@ class Agent(object):
 		toss = random()
 
 		# explore if toss is more than threshold, else choose next move greedy-ly
-		if toss > behaviour_threshold:
+		if toss > self.behaviour_threshold:
 			i, j = self.explore(state)
 		else:
 			i, j = self.greedy(state)
 
 		#set the current state and update the previous state
-		self.state[i][j] = self.symbol
+		state[i][j] = self.symbol
 		self.prev_state = state
 
 		return i, j # return the chosen move
@@ -189,7 +196,7 @@ class Agent(object):
     	
 		open_cells = open_spots(state)
 		random_choice = choice(open_cells)
-		return random_choice[0], random_choice[1]
+		return random_choice / len(state), random_choice % len(state)
 
 	def greedy(self, state):
 		max_value = -1*np.inf
@@ -200,15 +207,34 @@ class Agent(object):
 			for j in xrange(len(state[i])):
 				potential_state = deepcopy(state)
 				potential_state[i][j] = self.symbol
-				val = self.state_values[get_state_key(potential_state)]
-				if val > max_value:
-					max_value = val
-					best_move = (i, j)
-
+				
+				#putting this in a try block because we may encounter states not generated before
+				try:
+					val = self.state_values[get_state_key(potential_state)]
+					if val > max_value:
+						max_value = val
+						best_move = (i, j)
+				except KeyError:
+					#exit("State not seen before")
+					print(get_state_key(potential_state))
+					pdb.set_trace()
 		self.update_state_table(max_value)
 		return best_move[0], best_move[1]
 
 
-player1 = Agent(symbol=1)
-#player2 = Agent(symbol=-1)
-print "\nNumber of possible states: ", player1.count_states()
+def driver():
+	player1 = Agent(symbol=1)
+	player2 = Agent(symbol=-1)
+	#print("\nNumber of possible states: ", player1.count_states())
+
+	num_games = 10
+	for i in xrange(num_games):
+		winner = play(player1, player2)
+		if winner == 0:
+			print("Game ended in a draw!")
+		else:
+			if winner == 1:
+				player_symbol = "X"
+			else:
+				player_symbol = "O"
+			print(player_symbol, " won!")

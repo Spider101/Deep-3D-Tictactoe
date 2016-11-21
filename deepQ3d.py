e12b27476de96dbe4ab9c2f58385b6ff9b2eceaf
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2016-11-21 12:57:32 -0500

    added function to check if game over or not

---
+++ deepQ3d.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+ticTacToeShape = (3,3,3)
+
+
+def checkIfGameOver(state):
+    """
+    check if a game is over.
+    This function will go to each element, and check if its an 'x' or an 'o'
+
+    :param state: a valid state with strictly 3 dimensions, but can have any shape
+    :return: true if game is over , false if game is not over
+    """
+    for i in xrange(ticTacToeShape[0]):
+        for j in xrange(ticTacToeShape[1]):
+            for k in xrange(ticTacToeShape[2]):
+                if state[i][j][k] != 'x' and state[i][j][k] != 'o':
+                    return False
+    return True
+
+
+model = Sequential()
+model.add(Dense(256,input_dim=ticTacToeShape[0]*ticTacToeShape[1]*ticTacToeShape[2]))
+model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])

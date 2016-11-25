db31a57fdeb10b17a2843c82973d1628b5a0275b
Abhimanyu Banerjee <ab3cb@power1.cs.virginia.edu>
2016-11-25 15:15:52 -0500

    completed logic for qlearning testing and learning. TODO: model not learning

--- q_learning.py
+++ q_learning.py
@@ -2,50 +2,66 @@ from __future__ import print_function
 __author__ = "Abhimanyu Banerjee"
 
 from random import random, choice
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import timeit
 from sys import exit
 from copy import deepcopy
 import pdb
+import pickle
 
 from games import tttAgent2D
 from games.board import empty_state, is_game_over
 
-def play(player1, player2):
-	state = empty_state()
-	num_cells = len(state)*len(state[0])
-	for turn in range(num_cells):
-		if turn % 2 == 0:
-			i, j = player1.action(deepcopy(state))
-			symbol = player1.symbol
-		else:
-			i, j = player2.action(deepcopy(state))
-			symbol = player2.symbol
-		state[i][j] = symbol
-		winner = int(is_game_over(state))
-		if winner != 0: #one of the players won
-			return winner 
+def measure_performance(player1, player2, num_games):
 	
-	return winner #nobody won
-
-def driver():
-	player1 = tttAgent2D(symbol=1)
-	player2 = tttAgent2D(symbol=-1)
-	#print("\nNumber of possible states: ", player1.count_states())
-
-	num_games = 1000
+	probs = [0,0,0]
 	for i in range(num_games):
 		print("\nStarting game", i+1)
-		winner = play(player1, player2)
+		winner = player1.self_play(player2)
 		if winner == 0:
 			print("Game ended in a draw!")
+			probs[1] += 1.0/num_games
 		else:
 			if winner == 1:
 				player_symbol = "X"
+				probs[2] += 1.0/num_games
 			else:
 				player_symbol = "O"
+				probs[0] += 1.0/num_games
+			
 			print(player_symbol, " won!")
+
+	return probs
+
+def driver():
+	
+	player1 = tttAgent2D(symbol=1, behaviour_threshold=0.8)
+	player2 = tttAgent2D(symbol=-1, behaviour_threshold=0.0)
+	batches, batch_probs = 1000, []
+
+	for i in range(batches):
+		print("\nStarting batch", i)
+		batch_probs.append(measure_performance(player1, player2, 100))
+	
+	epochs = [i for i in range(batches)]
+	win_probs = [probs[2] for probs in batch_probs]
+	plt.plot(epochs, win_probs, label="Win Probability", color="g")
+	plt.xlabel('Epochs')
+	plt.ylabel('Probability')
+
+	behaviour_threshold = player1.behaviour_threshold if player1.behaviour_threshold != 0 else player2.behaviour_threshold
+	plt.title('RL Agent Performance vs. Random Agent\n({0} behaviour_threshold)'.format(behaviour_threshold))
+	plt.legend()
+	plt.savefig('selfplay_threshold_{0}.png'.format(behaviour_threshold))
+
+	if player1.behaviour_threshold != 0.0:
+		pickle.dump(player1.state_values, open("state_table_X.p", "wb"))
+
+	if player2.behaviour_threshold != 0.0:
+		pickle.dump(player2.state_values, open("state_table_O.p", "wb"))
 
 driver()
 

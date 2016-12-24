15dd33bda7a16791aecc5c863dd84dd430842079
Spider101 <banerjee.abhimanyu3@gmail.com>
2016-12-24 09:17:31 -0500

    modified learner interface to allow for multiple board formats and dimensions

--- td_learning_eval.py
+++ td_learning_eval.py
@@ -41,20 +41,21 @@ def measure_performance(player1, player2, num_games):
 
 if __name__ == "__main__":
 	
-	player1 = TDAgent(symbol=1, behaviour_threshold=0.05, dims=2)
-	player2 = TDAgent(symbol=-1, behaviour_threshold=0.05, dims=2)
+	player1 = TDAgent(symbol=1, behaviour_threshold=0.05, dims=2, build_states=False)
+	player2 = TDAgent(symbol=-1, behaviour_threshold=0.05, dims=2, build_states=False)
 	
-	player1.reset_agent()
-	player2.reset_agent()
+	#player1.reset_agent()
+	#print(player1.count_states())
+	#player2.reset_agent()
 
-	batches, batch_probs = 5000, []
+	batches, batch_probs = 50000, []
 	for i in range(batches):
 		print("\nStarting batch", i)
-		if i % 10 == 0:
+		if i % 100 == 0:
 			batch_probs.append(measure_performance(deepcopy(player1), deepcopy(player2), 100))
 		player1.self_play(player2)
 
-	epochs = [i for i in range(int(batches/10))]
+	epochs = [i for i in range(int(batches/100))]
 	win_probs = [probs[2] for probs in batch_probs]
 	
 	plt.plot(epochs, win_probs, label="Win Probability", color="g")
@@ -64,13 +65,13 @@ if __name__ == "__main__":
 	behaviour_threshold = player1.behaviour_threshold if player1.behaviour_threshold != 0 else player2.behaviour_threshold
 	plt.title('RL Agent Performance vs. Random Agent\n({0} behaviour_threshold)'.format(behaviour_threshold))
 	plt.legend()
-	plt.savefig('selfplay_threshold_{0}.png'.format(behaviour_threshold))
+	plt.savefig(join('results', '4x4', 'selfplay_threshold_{0}.png'.format(behaviour_threshold)))
 
 	if player1.behaviour_threshold != 0.0:
-		pickle.dump(player1.state_values, open(join("agents","state_table_X.p"), "wb"))
+		pickle.dump(player1.state_values, open(join("agents","state_table_{}x{}_X.p".format(4, 4)), "wb"))
 
 	if player2.behaviour_threshold != 0.0:
-		pickle.dump(player2.state_values, open(join("agents","state_table_O.p"), "wb"))
+		pickle.dump(player2.state_values, open(join("agents","state_table_{}x{}_X.p".format(4, 4)), "wb"))
 
 player1 = Agent(symbol=1)
 # player2 = Agent(symbol=-1)
